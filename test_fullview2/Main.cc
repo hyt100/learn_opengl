d3f8fff94c9603a82155ef0ac42176405aa4a781
hyt100 <hyt100_code@126.com>
2021-05-16 21:04:00 +0800

    add test_fullview2

--- test_fullview2/Main.cc
+++ test_fullview2/Main.cc
@@ -66,7 +66,8 @@ int main()
         // ------
         glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glEnable(GL_DEPTH_TEST);
         render.setRotate(pitch, yaw);
         render.draw();
 
